d9feccb6f0e69e9fd429c237a8dffdd066bf98e3
i211879 <philippe.parmentier.external@banque-france.fr>
2025-01-18 22:38:11 +0100

    preparation before timeline

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-gsap.to("ul li:last-child", {
+gsap.from("body", { backgroundColor: "#fff", duration: 1.7, ease: "none" });
+
+gsap.fromTo(
+  ["h1", ".intro"],
+  { opacity: 0, y: -20 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.6,
+    ease: "power1.out",
+    delay: 1.5,
+    stagger: 0.2,
+  }
+);
+
+gsap.from(["img", "h2"], {
+  opacity: 0,
   duration: 0.7,
-  y: 40,
-  ease: "power2.out",
-  repeat: -1,
-  reapeatDelay: 1,
-  yoyo: true,
-  yoyoEase: "none",
+  ease: "none",
+  delay: 2.8,
 });
+
+gsap.fromTo(
+  "ul li",
+  { opacity: 0, y: -20 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.6,
+    ease: "power1.out",
+    delay: 4,
+    stagger: 0.2,
+  }
+);
